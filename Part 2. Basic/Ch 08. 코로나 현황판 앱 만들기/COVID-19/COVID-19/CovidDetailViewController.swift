4c4bba776a7cf3539ce698a0852f05ba933e62ff
gwangyong <key4168@icloud.com>
2021-11-26 18:23:36 +0900

    feat: CH08_04 ~ CH08_05 그래프 차트 라이브러리, 기본 UI 구성 Alamofire, Charts 프레임워크 cocoapods로 설치 간단한 화면구성

---
+++ Part 2. Basic/Ch 08. 코로나 현황판 앱 만들기/COVID-19/COVID-19/CovidDetailViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+    
+    
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var overseasInfolwCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
