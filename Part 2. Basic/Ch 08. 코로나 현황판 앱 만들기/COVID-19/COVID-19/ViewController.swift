1b5616514b76b1842f9f73cf92a3bad217774951
gwangyong <key4168@icloud.com>
2021-11-26 18:36:41 +0900

    원래대로

--- Part 2. Basic/Ch 08. 코로나 현황판 앱 만들기/COVID-19/COVID-19/ViewController.swift
+++ Part 2. Basic/Ch 08. 코로나 현황판 앱 만들기/COVID-19/COVID-19/ViewController.swift
@@ -1,14 +1,17 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by 서광용 on 2021/11/26.
+//
+
 import UIKit
-import Charts
 
 class ViewController: UIViewController {
-    @IBOutlet weak var totalCaseLabel: UILabel!
-    @IBOutlet weak var newCaseLabel: UILabel!
-    @IBOutlet weak var pieChartView: PieChartView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Do any additional setup after loading the view.
     }
 
 
