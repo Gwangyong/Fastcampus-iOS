279c7afb8961b9f4491f96ca734429814db4c6cf
gwangyong <key4168@icloud.com>
2021-11-26 17:00:22 +0900

    COVID-19 앱 프로젝트 생성

---
+++ Part 2. Basic/Ch 08. 코로나 현황판 앱 만들기/COVID-19/COVID-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID-19
+//
+//  Created by 서광용 on 2021/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
