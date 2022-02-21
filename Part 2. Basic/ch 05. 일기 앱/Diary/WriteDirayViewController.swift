cd18dcf573d95dd89932a54557dc45b1c264d55e
gwangyong <key4168@icloud.com>
2022-02-21 11:37:21 +0900

    feat: 일기 작성의 날짜칸 기능 추가 날짜 칸을 누르면, DatePicker이 나오도록 하여 날짜를 선택할 수 있음 한국어로 설정하고, 날짜가 변경될 때마다 함수가 호출되서 날짜 칸의 값이 변경됨 비어있는 화면을 누르면 날짜를 선택하는 키보드가 내려감

--- Part 2. Basic/ch 05. 일기 앱/Diary/WriteDirayViewController.swift
+++ Part 2. Basic/ch 05. 일기 앱/Diary/WriteDirayViewController.swift
@@ -7,10 +7,16 @@ class WriteDirayViewController: UIViewController {
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var confirmButton: UIBarButtonItem!
     
+    private let datePicker = UIDatePicker()
+    private var dirayDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureContentsTextView()
+        self.configureDatePicker()
     }
+    
+
     
     // 내용의 TextView 테두리 색상등 설정
     private func configureContentsTextView() {
@@ -20,6 +26,32 @@ class WriteDirayViewController: UIViewController {
         self.contentsTextView.layer.cornerRadius = 5.0
     }
     
+    private func configureDatePicker() {
+        // 시간은 필요 없으니, 날짜만 표시되도록 .date
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        // addTarget는 UI 컨트롤러 객체가 이벤트에 응답하는 방식을 설정해주는 메소드
+        // 해당 뷰 컨트롤러에 해당되니 첫 매개변수는 self임
+        // datePickerValueDidChange가 값이 바뀔 때마다 datePickerValueDidChage가 호출됨
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        self.dateTextField.inputView = self.datePicker
+        
+    }
+    
     @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formmater = DateFormatter()
+        // E를 5번 적으면, 요일을 한 글자만 표현하게 할 수 있다함.
+        formmater.dateFormat = "yyyy년 mm월 dd일(EEEEE)"
+        formmater.locale = Locale(identifier: "ko_KR")
+        self.dirayDate = datePicker.date
+        self.dateTextField.text = formmater.string(from: datePicker.date)
+    }
+    
+    // 날짜 wheels에서 정하다가, 비어있는 화면들을 누르면 키보드가 내려가도록 하는 코드
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
