f13e46ec4fe24b1c26de7fb21a00891d24055498
gwangyong <key4168@icloud.com>
2022-02-25 21:22:15 +0900

    feat: UserDefaults를 통한 데이터 저장 UserDefaults를 사용해서 일기에 들어가는 내용들을  저장하여, 껐다켜도 데이터가 유지 앱을 껐다가 켜도 다시 저장된 데이터가 load될 수 있도록 기능 추가

--- Part 2. Basic/ch 05. 일기 앱/Diary/ViewController.swift
+++ Part 2. Basic/ch 05. 일기 앱/Diary/ViewController.swift
@@ -4,11 +4,18 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var diaryList = [Diary]()
+    private var diaryList = [Diary]() {
+        // 프로퍼티 옵저버를 사용해서, diaryList 배열에 값이 변경될 때 마다 값이 UserDefaults에 저장됨.
+        // didSet 옵저버를 사용했기 때문에, 값이 변경된 후에 실행됨
+        didSet {
+            self.saveDiaryList()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureCollectionView()
+        self.loadDiaryList()
     }
     
     // diaryList에 추가된 내용을 CollectionView에 표시 되도록 하는 코드
@@ -24,6 +31,34 @@ class ViewController: UIViewController {
         if let writeDiaryViewController = segue.destination as? WriteDirayViewController {
             // self 를 대입시켜줘서 delegate 를 위임을 받는다..?
             writeDiaryViewController.delegate = self
+        }
+    }
+    
+    private func saveDiaryList() {
+        let date = self.diaryList.map {
+            [
+                "title": $0.title,
+                "contents": $0.contents,
+                "date": $0.date,
+                "isStar": $0.isStar
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        // 아래에서 date는 기본 데이터베이스에 저장할 객체이고, diaryList는 값을 연결할 키이다.
+        userDefaults.set(date, forKey: "diaryList")
+    }
+    
+    private func loadDiaryList() {
+        let userDefaults = UserDefaults.standard
+        // object 메소드는 Any 타입으로 반환하기 때문에, 아래처럼 타입 캐스팅을 해주어야함.
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        self.diaryList = data.compactMap {
+            // 불러온 값이 Any 타입이므로, 각각에 맞게 as? 를 사용하여 타입 캐스팅
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
         }
     }
     
