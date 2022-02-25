fd155d2ea5497a8652b8388806c16e19865cb61d
gwangyong <key4168@icloud.com>
2022-02-25 22:43:05 +0900

    feat: 일기 목록의 각 cell에 테두리를 만들어줌

--- Part 2. Basic/ch 05. 일기 앱/Diary/DiaryCell.swift
+++ Part 2. Basic/ch 05. 일기 앱/Diary/DiaryCell.swift
@@ -1,7 +1,15 @@
 import UIKit
 
 class DiaryCell: UICollectionViewCell {
-    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    // 이 생성자는 UIView가 xib나 storyboard 에서 생성이 될 때, 이 생성자를 통해 객체가 생성됨
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        // cell의 테두리를 그려주는 코드
+        self.contentView.layer.cornerRadius = 3.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+    }
 }
